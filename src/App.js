e5513558dad1dc37b255fec4093df2a8cea0f919
dan <danmorro59@yahoo.com>
2023-03-08 13:18:41 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import Navbar from "./components/navbar/Navbar";
+import Secondary from "./components/Secondary/Secondary";
+import { MetubeProvider } from "./context/MetubeContext";
+import Video from "./components/video_component/Video";
+import Details from "./components/details/Details";
+function App() {
+  return (
+    <>
+      <MetubeProvider>
+        <Navbar />
+        <Video />
+        <Details />
+        {/* <Secondary /> */}
+      </MetubeProvider>
+    </>
+  );
+}
+
+export default App;
+
